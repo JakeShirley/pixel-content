80ebd7b59fb4e1babc927d1453b3ecc310e8cedd
Jake Shirley <jake@xbox.com>
2026-07-17 18:48:53 -0700

    Changed base URI to GitHub.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import pixelWallIntegration from './astro-integration.mjs';
 
 export default defineConfig({
-  site: 'https://jakeshirley.com',
+  site: 'https://jakeshirley.github.io',
   base: '/pixel-content',
   outDir: './_site',
   build: {
